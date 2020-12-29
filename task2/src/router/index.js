dcbab94aa0b4e9f21f2ea869b67c14999fa78c76
JackScher <dzhek.layt298@gmail.com>
2020-12-29 19:11:24 +0300

    deleted useless components/views

--- task2/src/router/index.js
+++ task2/src/router/index.js
@@ -1,9 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-// import Home from '../views/Home.vue'
-// import Confirm from '../views/Confirm.vue'
-// import LogedIn from "@/views/LogedIn";
-// import ProfileView from "@/views/ProfileView";
+
 import BaseView from "@/views/rework/entrance/BaseView";
 import LoginView from "@/views/rework/entrance/LoginView";
 import RegisterView from "@/views/rework/entrance/RegisterView";
@@ -203,39 +200,6 @@ const routes = [
   },
 ]
 
-
-
-
-// const routes = [
-//   {
-//     path: '/',
-//     name: 'Home',
-//     component: Home
-//   },
-//   {
-//     path: '/about',
-//     name: 'About',
-//     // route level code-splitting
-//     // this generates a separate chunk (about.[hash].js) for this route
-//     // which is lazy-loaded when the route is visited.
-//     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-//   },
-//   {
-//     path: '/verify-email',
-//     name: 'Confirm',
-//     component: Confirm
-//   },
-//   {
-//     path: '/logedin',
-//     name: 'LogedIn',
-//     component: LogedIn
-//   },
-//   {
-//     path: '/profile',
-//     name: 'Profile',
-//     component: ProfileView
-//   },
-// ]
 
 const router = new VueRouter({
   routes,
